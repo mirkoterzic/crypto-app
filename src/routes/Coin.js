b3bf8c4e0ffddfc70be23de0c99f94b6fddd2311
mirkoterzic <mirko.terzic2002@gmail.com>
2024-08-31 13:05:06 +0200

    depending on the id of the coin the user clicks specific data wll be on the page - using coingeckoapi to fetch data using the coin.id

--- src/routes/Coin.js
+++ src/routes/Coin.js
@@ -1,9 +1,24 @@
-import React from "react";
+import axios from "axios";
+
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Coin = () => {
+  const params = useParams();
+  const [coin, setCoin] = useState({});
+  const url = `${process.env.REACT_APP_API_COIN_URL}${params.coinId}`;
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setCoin(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <div>
-      <h1></h1>
+      <h1>{coin.id}</h1>
     </div>
   );
 };
