b9bf29304cd4ce1b79e1539549cff6fd7512c55e
mirkoterzic <mirko.terzic2002@gmail.com>
2024-09-01 15:41:35 +0200

    added style so info is aligned

--- src/components/CoinItem.js
+++ src/components/CoinItem.js
@@ -4,7 +4,7 @@ import "./Coins.css";
 const CoinItem = (props) => {
   return (
     <div className="coin-row">
-      <p>{props.coins.market_cap_rank}</p>
+      <p className="rank">{props.coins.market_cap_rank}</p>
       <div className="img-symbol">
         <img src={props.coins.image} alt="" />
         <p>{props.coins.symbol.toUpperCase()}</p>
